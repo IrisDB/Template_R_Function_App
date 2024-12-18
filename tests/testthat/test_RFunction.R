c1a5ac0998ed06922101da056eec12e1a36338db
IrisDB <idbontekoe@hotmail.com>
2024-12-18 14:44:48 +0100

    Update tests

--- tests/testthat/test_RFunction.R
+++ tests/testthat/test_RFunction.R
@@ -1,11 +1,18 @@
-test_data <- test_data("input3_move2loc_LatLon.rds")
+test_data <- test_data("input4_move2loc_LatLon.rds")
 
 test_that("happy path", {
-  actual <- rFunction(data = test_data, sdk = "unit test", year = 1998)
-  expect_equal(unique(lubridate::year(mt_time(actual))), 1998)
+  actual <- rFunction(data = test_data, sdk = "unit test", year = 2014)
+  expect_equal(unique(lubridate::year(mt_time(actual))), 2014)
 })
 
 test_that("year not included", {
   actual <- rFunction(data = test_data, sdk = "unit test", year = 1900)
   expect_null(actual)
+})
+
+test_that("App creates artifact", {
+  on.exit(file.remove("./data/output/plot.png"), add = TRUE)
+  rFunction(data = test_data, sdk = "unit test", year = 2014)
+  # Check if the file exists
+  expect_true(file.exists("./data/output/plot.png"))
 })
